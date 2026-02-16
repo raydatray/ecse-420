12f29a5f8e9f864ae834c89f1e5f0792a427306e
raydatray <raydatray@gmail.com>
2026-02-15 19:24:08 -0500

    [a2][2/n][bakery lock] impl
    Summary:
    
    Test Plan:

---
+++ a2/bakery-lock/src/main/java/solution/BakeryLock.java
@@ -0,0 +1,94 @@
+package solution;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.stream.IntStream;
+
+public class BakeryLock implements Lock {
+
+    private final AtomicIntegerArray flag;
+    private final AtomicIntegerArray label;
+
+    private final ThreadLocal<Integer> threadId = new ThreadLocal<>();
+    private final AtomicInteger idGenerator = new AtomicInteger(0);
+
+    public BakeryLock(Integer n) {
+        this.flag = new AtomicIntegerArray(n);
+        this.label = new AtomicIntegerArray(n);
+    }
+
+    public void lock() {
+        int n = flag.length();
+        int id = getOrSetThreadId();
+
+        flag.set(id, 1);
+
+        int highestLabel = IntStream.range(0, n)
+            .map(k -> label.get(k))
+            .max()
+            .orElse(0);
+
+        label.set(id, highestLabel + 1);
+        flag.set(id, 0);
+
+        System.out.printf("thread %d obtained label%d%n", id, label.get(id));
+
+        IntStream.range(0, n)
+            .filter(k -> k != id)
+            .forEach(k -> {
+                while (flag.get(k) == 1) {
+                    Thread.onSpinWait();
+                }
+
+                while (
+                    label.get(k) != 0 &&
+                    (label.get(k) < label.get(id) || (label.get(k) == label.get(id) && k < id))
+                ) {
+                    Thread.onSpinWait();
+                }
+            });
+
+        System.out.printf(
+            "thread %d with label %d has entered the CRITICAL SECTION%n",
+            id,
+            label.get(id)
+        );
+    }
+
+    public void unlock() {
+        int id = getOrSetThreadId();
+
+        flag.set(id, 0);
+        label.set(id, 0);
+    }
+
+    public void lockInterruptibly() {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    public Condition newCondition() {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    public boolean tryLock() {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    public boolean tryLock(long time, TimeUnit unit) {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    private Integer getOrSetThreadId() {
+        Integer id = threadId.get();
+
+        if (id == null) {
+            id = idGenerator.getAndIncrement();
+            threadId.set(id);
+        }
+
+        return id;
+    }
+}
