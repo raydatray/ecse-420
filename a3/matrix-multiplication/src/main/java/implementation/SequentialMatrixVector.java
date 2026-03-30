e340c058d8f9d60a2837489c9d177684d1fc7cd5
Ray Liu <raydatray@gmail.com>
2026-03-30 19:05:43 -0400

    [a3][8/n][matrix multplication] shelve sequential for full nuke
    Summary:
    
    Test Plan:

---
+++ a3/matrix-multiplication/src/main/java/implementation/SequentialMatrixVector.java
@@ -0,0 +1,18 @@
+package implementation;
+
+import java.util.stream.IntStream;
+
+public class SequentialMatrixVector {
+
+    public static double[] multiply(double[][] a, double[] x) {
+        int n = a.length;
+
+        return IntStream.range(0, n)
+            .mapToDouble(i ->
+                IntStream.Range(0, n)
+                    .mapToDouble(j -> a[i][j] * x[j])
+                    .sum()
+            )
+            .toArray();
+    }
+}
